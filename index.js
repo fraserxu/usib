a22713f50a7276d054e687816be4b789251d36bb
fraserxu <xvfeng123@gmail.com>
2013-09-01 16:00:04 +0800

    add test and a few improvement

--- index.js
+++ index.js
@@ -14,133 +14,126 @@ var imgur = require('imgur-node-api'),
 var args = process.argv.slice(2),
     argslen = args.length;
 
-var usib = (function() {
+module.exports = Usib;
 
-  var _key = '',
-      _keyloc = process.env.HOME + '/.imgurkey';
+/**
+ * Create usib object.
+ * @param {string} key imgur key
+ */
+function Usib(key) {
+  if (!(this instanceof Usib)) return new Usib(key);
 
-  // Get saved API key (~/.imgurkey)
-  function _getKey () {
-      // Synchronous since nothing can happen without it
-      if (!fs.existsSync(_keyloc)) {
-          return false;
-      }
-
-      return fs.readFileSync(_keyloc, 'utf8').replace(/[\n\r\t\s]/gm, '');
-  }
-
-  // Set an API key
-  function _setKey (key, persist) {
-      if (!key) { return; }
-      _key = key;
-      exports.key = key;
-      if (persist) {
-          fs.writeFile(_keyloc, key, 'utf8', function (err) {
-              if (err) {
-                  throw err;
-              }
-          });
-      }
-  }
-
-  /**
-   * take a screenshot with the given url
-   * @param  {string}   url    page url
-   * @param  {string}   imgPath the
-   * @param  {object}   options   screen options
-   * @return {string}   file   the path to the img
-   */
-  function _capture (url, imgPath, opts, callback) {
-    screenshot(url, opts)
-      .capture(function(err, img) {
-        if (err) throw err;
-        fs.writeFile(imgPath, img, function(err) {
-          if (err) throw err;
-          if (callback) callback(imgPath);
-        });
-      });
-  }
-
-  /**
-   * upload img and open in the browser
-   * @param {string} api_key for imgur
-   * @param  {string} file loaction to the image
-   */
-  function _upload (file, api_key) {
-    imgur.setClientID(api_key);
-    imgur.upload(file, function(err, res) {
-      child = exec('open ' + res.data.link, function(error, stdout, stderr) {
-        if(error) return console.log(clc.red(error));
-        console.log(clc.green('Your img url is: ' + res.data.link));
-      });
-    });
-  }
-
-  return {
-    'getKey': _getKey,
-    'setKey': _setKey,
-    'upload': _upload,
-    'capture': _capture
-  };
-
-}());
-
-exports = {
-  getKey : usib.getKey,
-  setKey: usib.setKey,
-  upload: usib.upload,
-  capture: usib.capture
+  this.keyloc = process.env.HOME + '/.imgurkey';
+  if(key) this.setKey(key, true);
 }
 
+/**
+ * Get saved API key (~/.imgurkey)
+ * @return {[type]} [description]
+ */
+Usib.prototype.getKey = function() {
+  if (!fs.existsSync(this.keyloc)) {
+    return false;
+  }
 
-// Commands
+  return fs.readFileSync(this.keyloc, 'utf8').replace(/[\n\r\t\s]/gm, '');
+}
+
+// Set an API key
+/**
+ * Set an API Key
+ * @param {string} key imgur key
+ * @param {bollean} persist true
+ */
+Usib.prototype.setKey = function(key, persist) {
+  if (!key) { return; }
+  this.key = key;
+  if (persist) {
+    fs.writeFileSync(this.keyloc, this.key, 'utf8');
+  }
+}
+
+/**
+ * take a screenshot with the given url
+ * @param  {string}   url    page url
+ * @param  {string}   imgPath the
+ * @param  {object}   options   screen options
+ * @return {string}   file   the path to the img
+ */
+Usib.prototype.capture = function(url, imgPath, opts, callback) {
+  screenshot(url, opts)
+    .capture(function(err, img) {
+      if (err) throw err;
+      fs.writeFile(imgPath, img, function(err) {
+        if (err) throw err;
+        if (callback) callback(imgPath);
+      });
+    });
+}
+
+/**
+ * upload img and open in the browser
+ * @param  {string} file loaction to the image
+ */
+Usib.prototype.upload = function(file) {
+  imgur.setClientID(this.key);
+  imgur.upload(file, function(err, res) {
+    console.log(clc.green('Your img url is: ' + res.data.link));
+    child = exec('open ' + res.data.link, function(error, stdout, stderr) {
+      if(error) return console.log(clc.red(error));
+    });
+  });
+}
+
+// Commandline use
 if (argslen) {
   var key = '';
+  var usib = new Usib();
 
   // Check if key is being set via cli
   if (args[0] === '-k') {
-      if (typeof args[1] === 'undefined') {
-          console.log(clc.yellow('Please specify a key, e.g: usib -k <key>'));
-          return;
-      }
+    if (typeof args[1] === 'undefined') {
+        console.log(clc.yellow('Please specify a key, e.g: usib -k <key>'));
+        return;
+    }
 
-      key = args[1] ;
-      usib.setKey(key, true);
-      console.log(clc.green('Key set to %s', key));
-      return;
+    key = args[1] ;
+    usib.setKey(key, true);
+    console.log(clc.green('Key set to %s', key));
+    return;
   }
 
   key = usib.getKey();
 
   if (!key) {
-      console.log(clc.yellow('Please specify a key, e.g: usib -k <key>\n\nIf you don\'t have one get one at get one at http://imgur.com/register/api_anon'));
-      return;
+    console.log(clc.yellow('Please specify a key, e.g: usib -k <key>\n\nIf you don\'t have one get one at get one at http://imgur.com/register/api_anon'));
+    return;
   }
 
   // Upload a local img with a give path
   if (args[0] === '-u') {
     if (typeof args[1] === 'undefined') {
-        console.log(clc.yellow('Please specify a path the file, e.g: usib -u <file>'));
-        return;
+      console.log(clc.yellow('Please specify a path the file, e.g: usib -u <file>'));
+      return;
     }
 
     file = args[1];
-    usib.upload(file, key);
+    usib.upload(file);
     return;
   }
 
   // Capture a page and save it to local
   if (args[0] === '-c') {
     if (typeof args[1] === 'undefined') {
-        console.log(clc.yellow('Please specify a path the file, e.g: usib -u <file>'));
-        return;
+      console.log(clc.yellow('Please specify a path the file, e.g: usib -u <file>'));
+      return;
     }
 
     url = args[1];
     if(argslen >= 2) var imgPath = args[2] || './usib.png';
     if(argslen >= 3) var opts = args[3] || {};
     console.log(clc.green('Tring to capture the page...'));
-    usib.capture(url, imgPath, opts, function(imgPath) {
+    usib.capture(url, imgPath, opts, function() {
       console.log(clc.green('Saved img to %s', imgPath));
     });
     return;
@@ -149,8 +142,8 @@ if (argslen) {
   // Capture && Upload
   if (args[0] === '-cu') {
     if (typeof args[1] === 'undefined') {
-        console.log(clc.yellow('Please specify a path the file, e.g: usib -u <file>'));
-        return;
+      console.log(clc.yellow('Please specify a path the file, e.g: usib -u <file>'));
+      return;
     }
 
     url = args[1];
@@ -159,7 +152,7 @@ if (argslen) {
     console.log(clc.green('Tring to capture the page...'));
     usib.capture(url, imgPath, opts, function(imgPath) {
       console.log(clc.green('Saved img to %s', imgPath));
-      usib.upload(imgPath, key);
+      usib.upload(imgPath);
     });
     return;
   }
---
+++ test/index.js
@@ -0,0 +1,46 @@
+var test = require('tape');
+var Usib = require('../index.js');
+var exec = require('child_process').exec;
+
+test('Usib module', function(t) {
+  t.plan(2);
+  t.equal(typeof Usib, 'function');
+  t.equal(typeof Usib(), 'object');
+});
+
+test('A usib instance', function(t) {
+    var usib = new Usib('cca0e9d41a2d7c7');
+    t.equal(usib.key, 'cca0e9d41a2d7c7', 'has getKey method.');
+    t.equal(typeof usib.getKey, 'function', 'has getKey method.');
+    t.equal(typeof usib.setKey, 'function', 'has setKey method.');
+    t.equal(typeof usib.upload, 'function', 'has upload method.');
+    t.equal(typeof usib.capture, 'function', 'has capture method.');
+    child = exec('rm ' + usib.keyloc);
+    t.end();
+});
+
+test('setKey and getKey', function(t) {
+    var usib = new Usib('cca0e9d41a2d7c7');
+    t.equal(usib.getKey(), 'cca0e9d41a2d7c7', 'can set a key');
+    child = exec('rm ' + usib.keyloc, function(error, stdout, stderr) {
+        t.equal(usib.getKey(), 'false', 'return false when without setting key');
+    });
+    t.end();
+});
+
+test('capture', function(t) {
+    var usib = new Usib('cca0e9d41a2d7c7');
+    var url = 'http://xvfeng.me';
+    var imgPath = './usib.png';
+    usib.capture(url, imgPath, {}, function(data) {
+        t.equal(data, imgPath, 'callback argument should be equal to imgPath');
+    });
+    t.end();
+});
+
+test('upload', function(t) {
+    var usib = new Usib('cca0e9d41a2d7c7');
+    var file = '../caonima.jpg';
+    usib.upload(file);
+    t.end();
+})
